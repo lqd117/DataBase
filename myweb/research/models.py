f52628a6d70f4c4453e24d9873ff4e180a763715
qdl117 <30854411+qdl117@users.noreply.github.com>
2018-12-20 14:58:22 +0800

    1

---
+++ myweb/research/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+# Create your models here.
+
+# 申报人信息
+class Reporter_information(models.Model):
+    pass
+
+# 科研课题信息
+class Research_topic_information(models.Model):
+    pass
+
+# 获奖信息
+class Award_information(models.Model):
+    pass
+
+# 课题鉴定
+class Question_identification(models.Model):
+    pass
+
+# 学术活动
+class Academic_events(models.Model):
+    pass
+
+# 科技论文
+class Scientific_Papers(models.Model):
+    pass
+
+# 著作
+class Book(models.Model):
+    pass
+
+# 专利
+class Patent(models.Model):
+    pass
+
+# 用户
+class User(models.Model):
+    pass
+
+# 系统日志
+class System_log(models.Model):
+    pass
